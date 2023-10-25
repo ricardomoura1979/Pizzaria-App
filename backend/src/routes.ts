5b897f3cd5b806222ea6b8cc50948bac1b02d3f8
Ricardo Moura <ricardomoura1979@gmail.com>
2023-10-25 07:38:32 -0400

    Routes, Controlllers and Services

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,15 +1,19 @@
 
 // Importando as dependências
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
+import { CreateUserController } from './controllers/user/CreateUserController'
 
 // Criando o objeto de roteamento
 const router = Router();
 
-// Definindo rota GET '/teste'
-router.get('/teste', (req: Request, res: Response) => {
-  // Retornando uma resposta JSON com a propriedade 'ok' definida como true
-  return res.json({ ok: true });
-});
+// // Definindo rota GET '/teste'
+// router.get('/teste', (req: Request, res: Response) => {
+//   // Retornando uma resposta JSON com a propriedade 'ok' definida como true
+//   return res.json({ ok: true });
+// });
+
+// User Routes
+router.post('/users', new CreateUserController().handle)
 
 // Exportando o objeto de roteamento
 export { router };
