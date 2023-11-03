08c94d73e224b9917dbe60ab0407a5d30dd39045
Ricardo Moura <ricardomoura1979@gmail.com>
2023-11-03 06:38:56 -0400

    creating a login environment

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,35 +1,54 @@
 import prismaClient from "../../prisma";
 import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
 
-interface AuthRequest{
+interface AuthRequest {
     email: string;
     password: string;
 }
 
 
 
-class AuthUserService{
-    async execute({ email, password }: AuthRequest){
+class AuthUserService {
+    async execute({ email, password }: AuthRequest) {
         //console.log(email);
         //verificar se o email existe
 
         const user = await prismaClient.user.findFirst({
-            where:{
+            where: {
                 email: email
             }
         })
 
-        if(!user){
+        if (!user) {
             throw new Error("User/password incorrect")
         }
         // verificar senha correta 
         const passwordMatch = await compare(password, user.password)
 
-        if(!passwordMatch){
+        if (!passwordMatch) {
             throw new Error("User/Pass incorrect")
         }
 
-        return { ok: true }
+        // gerar token JWT e devolver os dados do usuario como id, name e email. Gerando token do usuario:
+
+        const token = sign(
+            {
+                name: user.name,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }
+        )
+
+        return { 
+            id: user.id,
+            email:user.email,
+            token: token
+        }
 
     }
 }
