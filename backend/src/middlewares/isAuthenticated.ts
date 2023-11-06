4f1a15e2af6c1a71c4ac9f1ae7f37d7ddbf2aa8f
Ricardo Moura <ricardomoura1979@gmail.com>
2023-11-06 07:11:57 -0500

    Token Validation

--- backend/src/middlewares/isAuthenticated.ts
+++ backend/src/middlewares/isAuthenticated.ts
@@ -1,3 +1,4 @@
+import { Payload } from '@prisma/client/runtime/library';
 import {NextFunction, Request, Response} from 'express'
 
 export function isAuthenticated(
@@ -5,7 +6,36 @@ export function isAuthenticated(
     res: Response,
     next: NextFunction 
 
-){
-    console.log("CHAMOU ESSE MIDDLEWARE")
+interface PayLoad{
+    sub: string;
 }
+
+){
+    // console.log("CHAMOU ESSE MIDDLEWARE")
+    // return next();
+
+    // receber o token
+    const authToken = req.headers.authorization;
+
+    if(!authToken){
+        return res.status(401).end();
+
+    }
+
+
+   const [, token] = authToken.split(" ")
+   
+   
+   try{
+    // validar esse token
+    const { sub } = verify(
+        token, 
+        process.env.JWT_SECRET
+    ) as Payload;
+
+   } catch(err){
+    return res.status(401).end();
+   }
+}
+
 
